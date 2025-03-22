6f58a32a2d0f36cfe79934eb5eab59de5364bc5b
karan.bharadwaj@coditas.com <karan.bharadwaj@coditas.com>
2025-03-22 22:38:12 +0530

    Added call popup, list and contact pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import Lara from '@primeng/themes/lara';
+
 
 import { routes } from './app.routes';
 
@@ -12,10 +12,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideAnimationsAsync(),
-        providePrimeNG({
-            theme: {
-                preset: Aura
-            }
-        })
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
   ]
 };
